4868fef372896c7174742f7c05a75dfc286f4be0
Levi <Daz>
2024-03-05 19:40:40 -0600

    agregar body parser y login.routes

--- HTML/app.js
+++ HTML/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use((request, response, next) => {
     console.log('Test');
     next();
